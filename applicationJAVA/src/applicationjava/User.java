1d992b6750d896e1891c585b818a8bd47c44aa00
Titiloss <Titiloss@Titiloss-PC>
2014-02-18 13:32:03 +0100

    c'est la 3ere modification mainben1

---
+++ applicationJAVA/src/applicationjava/User.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applicationjava;
+
+/**
+ *
+ * @author Titiloss
+ */
+public class User {
+    private int id;
+    
+}
